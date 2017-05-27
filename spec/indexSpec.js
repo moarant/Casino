296d814b5e01809451c617874105d9959a82524a
Molly Arant <mccormick.mollyt@gmail.com>
2017-05-27 17:16:57 -0400

    card deck is a work in progress

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,8 +1,14 @@
 const index = require("../src/js/index.js");
 
-describe("Cat gets Mouthy", function() {
-  it("Cat speak", function() {
-    console.log(index)
-    expect(index.catSpeak()).toBe("Cat says meow");
+describe("Deal a card", function(){
+  it("deal", function(){
+    expect(index.deal()).toBe(Card);
   })
 });
+
+// describe("Cat gets Mouthy", function() {
+//   it("Cat speak", function() {
+//     console.log(index)
+//     expect(index.catSpeak()).toBe("Cat says meow");
+//   })
+// });
--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,61 @@
 
 
-function catSpeak() {
-    return "Cat says meow";
-  }
+// function catSpeak() {
+//     return "Cat says meow";
+//   }
 
-  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    console.log(module.export)
-    let exports = module.exports= {}
-    exports.catSpeak = catSpeak;
-  }
-  else {
-    console.log("Else triggers")
-    window.catSpeak = catSpeak;
-  }
+ function Card (value, suit){
+   this.value= value;
+   this.suit= suit;
+ };
+
+function deckOfCards(){
+  //  this.deckOfCards= new Array(52);
+   var values= new Array("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace");
+   var suits= new Array("Diamonds", "Hearts", "Spades", "Clubs");
+   this.deckOfCards= new Array(52);
+   for(var i=0;i<13;i++){
+     for(var j=0;j<4;j++){
+       this.deckOfCards[i*suits.length+j]= new Card(values[j], suits[i]);
+     }
+   }
+ }
+  function shuffle(){
+    for(var i=0;i<5;i++){
+      for(var j=0;j<this.deckOfCards.length;j++){
+        var shuffledDeck= Math.floor(Math.random()*this.deckOfCards.length);
+        var temporaryDeck= this.deckOfCards[j];
+        this.deckOfCards[j]= this.deckOfCards[shuffledDeck];
+        this.deckOfCards[shuffledDeck]=temporaryDeck;
+      }
+    }
+  };
+
+  function deal(){
+    if (this.deckOfCards.length>0){
+      //remove from top of deck if there are cards available
+      return this.deckOfCards.shift();
+    }
+    else{
+      var error= "There are no more cards available in the deck";
+       return error;
+    }
+  };
+
+
+
+
+
+module.exports= {"deal": deal};
+//module.exports= {"catSpeak": catSpeak};
+
+
+  // if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  //   console.log(module.export)
+  //   let exports = module.exports= {}
+  //   exports.catSpeak = catSpeak;
+  // }
+  // else {
+  //   console.log("Else triggers")
+  //   window.catSpeak = catSpeak;
+  // }
